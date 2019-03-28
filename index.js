b7d8ddf5b51ec8df6c4666e228d6f208d2b56fd0
John Lennie <johnanthonylennie@gmail.com>
2019-03-28 14:19:50 -0400

    completed required features

--- index.js
+++ index.js
@@ -1,29 +1,88 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-class Withdrawal {
+  get balance() {
+    let balance = 0;
+    for (let transaction of this.transactions) {
+      balance += transaction;
+    }
+    return balance;
+  }
 
-  constructor(amount) {
+  get allTransactions() {
+    return this.transactions;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    if (!this.check()) {
+      return console.log("* unsuccessful transaction, not enough funds *\n");
+    }
+    console.log("* successful transaction *\n");
+    // Add the transaction to the account
+    this.account.addTransaction(this.value);
   }
-
 }
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+  check() {
+    return true;
+  }
+}
 
-
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  check() {
+    // if (this.value > this.account.balance) {
+    //   return false;
+    // }
+    if (this.amount > this.account.balance) {
+      return false;
+    }
+    return true;
+  }
+}
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("billybob");
+
+console.log("" + JSON.stringify(myAccount) + "\n");
+
+console.log("Starting Balance:", myAccount.balance, "\n");
+
+const t1 = new Deposit(120.0, myAccount);
+console.log("Transaction 1:", t1);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(120.0, myAccount);
+console.log("Transaction 2:", t2);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(120.0, myAccount);
+console.log("Transaction 3:", t3);
+t3.commit();
+
+console.log("All Transactions:", myAccount.allTransactions, "\n");
+
+console.log("Ending Balance:", myAccount.balance, "\n");
